d978ef50e5d3abab01cf049a1f3f15a037abec60
candranurfaritzi <125519911+candranurfaritzi@users.noreply.github.com>
2024-10-17 20:06:16 +0700

    Add files via upload

---
+++ MVC/app/models/User_model.php
@@ -0,0 +1,11 @@
+<?php
+
+class User_model
+{
+  private $nama = 'Rinaldi';
+
+  public function getUser()
+  {
+    return $this->nama;
+  }
+}
---
+++ MVC/app/views/about/index.php
@@ -0,0 +1,7 @@
+<div class="container">
+
+  <h1 class="mt-4">About Me</h1>
+  <img src="<?= BASEURL; ?>/img/profile.jpg" alt="candra" width="200" height="200" class="rounded-circle shadow object-fit-cover">
+  <p>Halo, nama saya <?= $data['nama']; ?>, umur saya <?= $data['umur']; ?> tahun, saya adalah seorang <?= $data['pekerjaan']; ?>.</p>
+
+</div>
